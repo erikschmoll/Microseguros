b569127c9997fe4e34fbaf8daabcb9a52347e2ba
Erik Schmoll <erikschmoll@gmail.com>
2019-01-28 12:19:22 -0300

    creo los proyectos.

---
+++ Microseguros.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microseguros.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Microseguros.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microseguros.Service
+{
+    public class Class1
+    {
+    }
+}
