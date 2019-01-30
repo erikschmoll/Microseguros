ac5b8e065650900bda74d0e1e7124742deacc229
Erik Schmoll <erikschmoll@gmail.com>
2019-01-29 21:21:57 -0300

    configuro serilog

--- Microseguros.Api/Program.cs
+++ Microseguros.Api/Program.cs
@@ -7,18 +7,37 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Microseguros.Api
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .Build();
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(Configuration)
+            .CreateLogger();
+            try
+            {
+                Log.Information("Getting the motors running...");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog();
     }
 }
--- Microseguros.Service/Business/DeviceService.cs
+++ Microseguros.Service/Business/DeviceService.cs
@@ -3,14 +3,17 @@ using Microseguros.Core.Business;
 using System;
 using System.Collections.Generic;
 using Microseguros.Core.DataAccess;
+using Microsoft.Extensions.Logging;
 
 namespace Microseguros.Business
 {
     public class DeviceService : IDeviceService
     {
         private readonly IDeviceRepository _deviceRepository;
-        public DeviceService(IDeviceRepository deviceRepository)
+        private readonly ILogger<DeviceService> _logger;
+        public DeviceService(ILogger<DeviceService> logger, IDeviceRepository deviceRepository)
         {
+            _logger = logger;
             _deviceRepository = deviceRepository;
         }
         public IEnumerable<Device> GetAll()
